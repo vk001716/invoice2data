f8f6bba3d6c8c9706ec215fc11bdd12b3d5293d7
Virat Kumar <36535270+vk001716@users.noreply.github.com>
2021-06-01 10:41:45 +0530

    Update tables.py

--- src/invoice2data/extract/plugins/tables.py
+++ src/invoice2data/extract/plugins/tables.py
@@ -7,13 +7,14 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-DEFAULT_OPTIONS = {"field_separator": r"\s+", "line_separator": r"\n"}
+DEFAULT_OPTIONS = {'field_separator': r'\s+', 'line_separator': r'\n'}
 
 
 def extract(self, content, output):
     """Try to extract tables from an invoice"""
-
-    for table in self["tables"]:
+    print("table plugin called")
+    i=0
+    for table in self['tables']:
 
         # First apply default options.
         plugin_settings = DEFAULT_OPTIONS.copy()
@@ -21,42 +22,40 @@ def extract(self, content, output):
         table = plugin_settings
 
         # Validate settings
-        assert "start" in table, "Table start regex missing"
-        assert "end" in table, "Table end regex missing"
-        assert "body" in table, "Table body regex missing"
+        assert 'start' in table, 'Table start regex missing'
+        assert 'end' in table, 'Table end regex missing'
+        assert 'body' in table, 'Table body regex missing'
 
-        start = re.search(table["start"], content)
-        end = re.search(table["end"], content)
+        start = re.search(table['start'], content)
+        end = re.search(table['end'], content)
 
         if not start or not end:
-            logger.warning("no table body found - start %s, end %s", start, end)
+            logger.warning('no table body found - start %s, end %s', start, end)
             continue
 
-        table_body = content[start.end() : end.start()]
-        logger.debug("START table body content ========================")
-        logger.debug(table_body)
-        logger.debug("END table body content ==========================")
-        logger.debug(f"Regex pattern = {table['body']}")
+        table_body = content[start.end(): end.start()]
 
-        for line in re.split(table["line_separator"], table_body):
+        for line in re.split(table['line_separator'], table_body):
             # if the line has empty lines in it , skip them
-            if not line.strip("").strip("\n") or not line:
+            if not line.strip('').strip('\n') or not line:
                 continue
 
-            match = re.search(table["body"], line)
+            match = re.search(table['body'], line)
             if match:
                 for field, value in match.groupdict().items():
                     # If a field name already exists, do not overwrite it
+                    print(field)
                     if field in output:
-                        continue
+                        i = i+1
+                        field = field + str(i)
 
-                    if field.startswith("date") or field.endswith("date"):
+                    if field.startswith('date') or field.endswith('date'):
                         output[field] = self.parse_date(value)
                         if not output[field]:
                             logger.error("Date parsing failed on date '%s'", value)
                             return None
-                    elif field.startswith("amount"):
+                    elif field.startswith('amount'):
                         output[field] = self.parse_number(value)
                     else:
                         output[field] = value
-            logger.debug("ignoring *%s* because it doesn't match anything", line)
+            logger.debug('ignoring *%s* because it doesn\'t match anything', line)
