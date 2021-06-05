85695385ca2ff0f789981a0cbda73047d0bd7648
Virat Kumar <36535270+vk001716@users.noreply.github.com>
2021-06-05 22:56:58 +0530

    Update invoice_template.py

--- src/invoice2data/extract/invoice_template.py
+++ src/invoice2data/extract/invoice_template.py
@@ -9,25 +9,23 @@ import dateparser
 from unidecode import unidecode
 import logging
 from collections import OrderedDict
-from . import parsers
 from .plugins import lines, tables
 
+from collections import defaultdict
 logger = logging.getLogger(__name__)
 
 OPTIONS_DEFAULT = {
-    "remove_whitespace": False,
-    "remove_accents": False,
-    "lowercase": False,
-    "currency": "EUR",
-    "date_formats": [],
-    "languages": [],
-    "decimal_separator": ".",
-    "replace": [],  # example: see templates/fr/fr.free.mobile.yml
+    'remove_whitespace': False,
+    'remove_accents': False,
+    'lowercase': False,
+    'currency': 'EUR',
+    'date_formats': [],
+    'languages': [],
+    'decimal_separator': '.',
+    'replace': [],  # example: see templates/fr/fr.free.mobile.yml
 }
 
-PARSERS_MAPPING = {"lines": parsers.lines, "regex": parsers.regex, "static": parsers.static}
-
-PLUGIN_MAPPING = {"lines": lines, "tables": tables}
+PLUGIN_MAPPING = {'lines': lines, 'tables': tables}
 
 
 class InvoiceTemplate(OrderedDict):
@@ -56,188 +54,182 @@ class InvoiceTemplate(OrderedDict):
         # Merge template-specific options with defaults
         self.options = OPTIONS_DEFAULT.copy()
 
-        for lang in self.options["languages"]:
-            assert len(lang) == 2, "lang code must have 2 letters"
+        for lang in self.options['languages']:
+            assert len(lang) == 2, 'lang code must have 2 letters'
 
-        if "options" in self:
-            self.options.update(self["options"])
+        if 'options' in self:
+            self.options.update(self['options'])
 
         # Set issuer, if it doesn't exist.
-        if "issuer" not in self.keys():
-            self["issuer"] = self["keywords"][0]
+        if 'issuer' not in self.keys():
+            self['issuer'] = self['keywords'][0]
 
-    def prepare_input(self, extracted_str: str) -> str:
+    def prepare_input(self, extracted_str):
         """
         Input raw string and do transformations, as set in template file.
         """
 
         # Remove withspace
-        if self.options["remove_whitespace"]:
-            optimized_str = re.sub(" +", "", extracted_str)
+        if self.options['remove_whitespace']:
+            optimized_str = re.sub(' +', '', extracted_str)
         else:
             optimized_str = extracted_str
 
         # Remove accents
-        if self.options["remove_accents"]:
+        if self.options['remove_accents']:
             optimized_str = unidecode(optimized_str)
 
         # convert to lower case
-        if self.options["lowercase"]:
+        if self.options['lowercase']:
             optimized_str = optimized_str.lower()
 
         # specific replace
-        for replace in self.options["replace"]:
-            assert len(replace) == 2, "A replace should be a list of exactly 2 elements."
-            optimized_str = re.sub(replace[0], replace[1], optimized_str)
+        for replace in self.options['replace']:
+            assert len(replace) == 2, 'A replace should be a list of 2 items'
+            optimized_str = optimized_str.replace(replace[0], replace[1])
 
         return optimized_str
 
-    def matches_input(self, optimized_str: str) -> bool:
-        """See if string matches all keyword patterns and no exclude_keyword patterns set in template file.
+    def matches_input(self, optimized_str):
+        """See if string matches keywords set in template file"""
 
-        Args:
-        optimized_str: String of the text from OCR of the pdf after applying options defined in the template.
-
-        Return:
-        Boolean
-            - True if all keywords are found and none of the exclude_keywords are found.
-            - False if either not all keywords are found or at least one exclude_keyword is found."""
-
-        if all([re.search(keyword, optimized_str) for keyword in self["keywords"]]):
-            # All keyword patterns matched
-            if self["exclude_keywords"]:
-                if any([re.search(exclude_keyword, optimized_str) for exclude_keyword in self["exclude_keywords"]]):
-                    # At least one exclude_keyword matches
-                    logger.debug("Template: %s. Keywords matched. Exclude keyword found!", self["template_name"])
-                    return False
-            # No exclude_keywords or none match, template is good
-            logger.debug("Template: %s. Keywords matched. No exclude keywords found.", self["template_name"])
+        if all([keyword in optimized_str for keyword in self['keywords']]):
+            logger.debug('Matched template %s', self['template_name'])
             return True
-        else:
-            logger.debug("Template: %s. Failed to match all keywords.", self["template_name"])
-            return False
 
     def parse_number(self, value):
         assert (
-            value.count(self.options["decimal_separator"]) < 2
-        ), "Decimal separator cannot be present several times"
+            value.count(self.options['decimal_separator']) < 2
+        ), 'Decimal separator cannot be present several times'
         # replace decimal separator by a |
-        amount_pipe = value.replace(self.options["decimal_separator"], "|")
+        amount_pipe = value.replace(self.options['decimal_separator'], '|')
         # remove all possible thousands separators
-        amount_pipe_no_thousand_sep = re.sub(r"[.,'\s]", "", amount_pipe)
+        amount_pipe_no_thousand_sep = re.sub(r'[.,\s]', '', amount_pipe)
         # put dot as decimal sep
-        return float(amount_pipe_no_thousand_sep.replace("|", "."))
+        return float(amount_pipe_no_thousand_sep.replace('|', '.'))
 
     def parse_date(self, value):
         """Parses date and returns date after parsing"""
         res = dateparser.parse(
-            value,
-            date_formats=self.options["date_formats"],
-            languages=self.options["languages"],
+            value, date_formats=self.options['date_formats'], languages=self.options['languages']
         )
         logger.debug("result of date parsing=%s", res)
         return res
 
     def coerce_type(self, value, target_type):
-        if target_type == "int":
+        if target_type == 'int':
             if not value.strip():
                 return 0
             return int(self.parse_number(value))
-        elif target_type == "float":
+        elif target_type == 'float':
             if not value.strip():
                 return 0.0
             return float(self.parse_number(value))
-        elif target_type == "date":
+        elif target_type == 'date':
             return self.parse_date(value)
-        assert False, "Unknown type"
+        assert False, 'Unknown type'
 
     def extract(self, optimized_str):
         """
         Given a template file and a string, extract matching data fields.
         """
 
-        logger.debug("START optimized_str ========================")
+        logger.debug('START optimized_str ========================')
         logger.debug(optimized_str)
-        logger.debug("END optimized_str ==========================")
+        logger.debug('END optimized_str ==========================')
         logger.debug(
-            "Date parsing: languages=%s date_formats=%s",
-            self.options["languages"],
-            self.options["date_formats"],
+            'Date parsing: languages=%s date_formats=%s',
+            self.options['languages'],
+            self.options['date_formats'],
         )
-        logger.debug(
-            "Float parsing: decimal separator=%s", self.options["decimal_separator"]
-        )
-        logger.debug("keywords=%s", self["keywords"])
+        logger.debug('Float parsing: decimal separator=%s', self.options['decimal_separator'])
+        logger.debug("keywords=%s", self['keywords'])
         logger.debug(self.options)
 
         # Try to find data for each field.
-        output = {}
-        output["issuer"] = self["issuer"]
+        #@vk001716
+        output = defaultdict()
+        output['issuer'] = self['issuer']
 
-        for k, v in self["fields"].items():
-            if isinstance(v, dict):
-                if "parser" in v:
-                    if v["parser"] in PARSERS_MAPPING:
-                        parser = PARSERS_MAPPING[v["parser"]]
-                        value = parser.parse(self, v, optimized_str)
-                        if value is not None:
-                            output[k] = value
-                        else:
-                            logger.error("Failed to parse field %s with parser %s", k, v["parser"])
-                    else:
-                        logger.warning("Field %s has unknown parser %s set", k, v["parser"])
-                else:
-                    logger.warning("Field %s doesn't have parser specified", k)
-            elif k.startswith("static_"):
+        for k, v in self['fields'].items():
+            if k.startswith('static_'):
                 logger.debug("field=%s | static value=%s", k, v)
-                output[k.replace("static_", "")] = v
+                output[k.replace('static_', '')] = v
             else:
-                # Legacy syntax support (backward compatibility)
                 logger.debug("field=%s | regexp=%s", k, v)
 
-                result = None
-                if k.startswith("sum_amount") and type(v) is list:
-                    k = k[4:]
-                    result = parsers.regex.parse(self, {"regex": v, "type": "float", "group": "sum"}, optimized_str,
-                                                 True)
-                elif k.startswith("date") or k.endswith("date"):
-                    result = parsers.regex.parse(self, {"regex": v, "type": "date"}, optimized_str, True)
-                elif k.startswith("amount"):
-                    result = parsers.regex.parse(self, {"regex": v, "type": "float"}, optimized_str, True)
+                sum_field = False
+                if k.startswith('sum_amount') and type(v) is list and False:
+                    k = k[4:]  # remove 'sum_' prefix
+                    sum_field = True
+                # Fields can have multiple expressions
+                if type(v) is list:
+                    res_find = []
+                    for v_option in v:
+                        res_val = re.findall(v_option, optimized_str)
+                        if res_val:
+                            if sum_field:
+                                res_find += res_val
+                            else:
+                                res_find.extend(res_val)
                 else:
-                    result = parsers.regex.parse(self, {"regex": v}, optimized_str, True)
-
-                if result is None:
+                    res_find = re.findall(v, optimized_str)
+                if res_find:
+                    logger.debug("res_find=%s", res_find)
+                    if (k.startswith('date') or k.endswith('date')) and False:
+                        output[k] = self.parse_date(res_find[0])
+                        if not output[k]:
+                            logger.error("Date parsing failed on date '%s'", res_find[0])
+                            return None
+                    elif (k.startswith('amount')) and False:
+                        if sum_field:
+                            output[k] = 0
+                            for amount_to_parse in res_find:
+                                output[k] += self.parse_number(amount_to_parse)
+                        else:
+                            output[k] = self.parse_number(res_find[0])
+                    else:
+                        res_find = list(set(res_find))
+                        if len(res_find) == 1:
+                            output[k] = res_find[0]
+                        else:
+                            output[k] = res_find
+                else:
                     logger.warning("regexp for field %s didn't match", k)
-                else:
-                    output[k] = result
 
-        output["currency"] = self.options["currency"]
-
+        
+	
+	
+        #output['currency'] = self.options['currency']
         # Run plugins:
         for plugin_keyword, plugin_func in PLUGIN_MAPPING.items():
             if plugin_keyword in self.keys():
                 plugin_func.extract(self, optimized_str, output)
+	# @vk001716
 
+        if len(output.keys()) > 0 :
+            return dict(output)
+        return None
+        
+        
+        
         # If required fields were found, return output, else log error.
-        if "required_fields" not in self.keys():
-            required_fields = ["date", "amount", "invoice_number", "issuer"]
+        if 'required_fields' not in self.keys():
+            required_fields = ['date', 'amount', 'invoice_number', 'issuer']
         else:
             required_fields = []
-            for v in self["required_fields"]:
+            for v in self['required_fields']:
                 required_fields.append(v)
 
         if set(required_fields).issubset(output.keys()):
-            output["desc"] = "Invoice from %s" % (self["issuer"])
+            output['desc'] = 'Invoice from %s' % (self['issuer'])
             logger.debug(output)
             return output
         else:
             fields = list(set(output.keys()))
             logger.error(
-                "Unable to match all required fields. "
-                "The required fields are: {0}. "
-                "Output contains the following fields: {1}.".format(
-                    required_fields, fields
-                )
+                'Unable to match all required fields. '
+                'The required fields are: {0}. '
+                'Output contains the following fields: {1}.'.format(required_fields, fields)
             )
             return None
