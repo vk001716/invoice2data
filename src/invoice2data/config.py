ee1e8bf0a862b23f22215af730d236681f862bf5
Virat Kumar <36535270+vk001716@users.noreply.github.com>
2021-06-09 15:24:45 +0530

    Create config.py

---
+++ src/invoice2data/config.py
@@ -0,0 +1,25 @@
+master_json = {
+    'issuer' : "*",
+    "keywords" : ["*"],
+    "fields" : [{
+        "Default" : ["Default Value 115"]
+    }],
+    "tables" : [
+        {
+            "start" : "Default Value ",
+            "end" : "Default Value",
+            "body" : "Default Value"
+        }
+    ]
+}
+removed_json = {
+    "Default" : "Default"
+}
+updated_json = {
+    "Default" : "Default"
+}
+master_json_file = 'master_json.json'
+master_template_folder = 'master_template'
+master_template_yml_file = 'master.yml'
+removed_json_file = 'removed_json.json'
+updated_json_file = 'updated_json.json'
